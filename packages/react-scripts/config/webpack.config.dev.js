407ab6f1aff7005fe3742323c97d9fe96aafe9d7
Ignacio Rivas <rivasign@gmail.com>
2017-10-12 14:52:08 +0200

    remove duped plugin

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -237,19 +237,14 @@ module.exports = {
                   plugins: () => [
                     require('postcss-import'),
                     require('postcss-mixins'),
-                    require('postcss-cssnext'),
+                    require('postcss-cssnext')({
+                      browsers:
+                        '> 1%, last 4 versions, Firefox ESR, not ie < 9',
+                      flexbox: 'no-2009',
+                    }),
                     require('postcss-nested'),
                     require('postcss-inline-svg'),
                     require('postcss-flexbugs-fixes'),
-                    autoprefixer({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
-                      ],
-                      flexbox: 'no-2009',
-                    }),
                   ],
                 },
               },
